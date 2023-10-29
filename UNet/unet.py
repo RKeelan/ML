01a69701f2ae30717dd391c1b2fdaf3770a921d7
R.Keelan <R.Keelan@gmail.com>
2023-10-28 22:02:51 -0400

    Add UNet
    - UNet implementation from https://github.com/milesial/Pytorch-UNet/

---
+++ UNet/unet.py
@@ -0,0 +1,426 @@
+import logging
+import numpy as np
+import os
+from pathlib import Path
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from tqdm import tqdm
+import wandb
+
+
+from UNet.utils import (
+    BasicDataset,
+    CarvanaDataset,
+    dice_coeff,
+    dice_loss,
+    multiclass_dice_coeff,
+    plot_img_and_mask
+)
+
+IMAGE_DIR = Path('./data/imgs/')
+MASK_DIR = Path('./data/masks/')
+CHECKPOINT_DIR = Path('./checkpoints/')
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+    
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.down3 = torch.utils.checkpoint(self.down3)
+        self.down4 = torch.utils.checkpoint(self.down4)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.up3 = torch.utils.checkpoint(self.up3)
+        self.up4 = torch.utils.checkpoint(self.up4)
+        self.outc = torch.utils.checkpoint(self.outc)
+
+
+def evaluate(model: nn.Module, dataloader, device: torch.device, amp: bool):
+    assert isinstance(model, UNet), "Evaluate only works with UNet right now"
+    model.eval()
+    num_val_batches = len(dataloader)
+    dice_score = 0
+
+    # iterate over the validation set
+    with torch.autocast(device.type if device.type != "mps" else "cpu", enabled=amp):
+        for batch in tqdm(dataloader, total=num_val_batches, desc="Validation round", unit="batch", leave=False):
+            image = batch['image']
+            mask_true = batch['mask']
+
+            # Move images and labels to correct device and type
+            image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            mask_true = mask_true.to(device=device, dtype=torch.long)
+
+            # Predict the mask
+            mask_pred = model(image)
+
+            if model.n_classes == 1:
+                assert mask_true.min() >= 0 and mask_true.max() <= 1, "True mask indices should be in [0, 1]"
+                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
+
+                # Compute the Dice score
+                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
+            else:
+                assert mask_true.min() >= 0 and mask_true.max() < model.n_classes, \
+                        "True mask indices should be in [0, n_classes)"
+                
+                # Convert to one-hot format
+                mask_true = F.one_hot(mask_true, model.n_classes).permute(0, 3, 1, 2).float()
+                mask_pred = F.one_hot(mask_pred.argmax(dim=1), model.n_classes).permute(0, 3, 1, 2).float()
+
+                # Compute the Dice score, ignoring background
+                dice_score += multiclass_dice_coeff(mask_pred[:, 1:], mask_true[:, 1:], reduce_batch_first=False)
+    
+    model.train()
+    return dice_score / max(num_val_batches, 1)
+
+
+# This split between "train_model" and "train_unet" doesn't necessarily make a lot of sense beucase I was originally
+# trying to factorize out a "common" train function for any model
+def train_model(
+        model: nn.Module,
+        device: torch.device,
+        dataset,
+        epochs: int = 5,
+        batch_size: int = 1,
+        learning_rate: float = 1e-5,
+        val_percent: float = 0.1,
+        save_checkpoint: bool = True,
+        amp: bool = False,
+        weight_decay: float = 1e-8,
+        momentum: float = 0.999,
+        gradient_clipping: float = 1.0,
+        extra_config: dict = {}
+    ):
+    # Split into train / validation partitions
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train_set, val_set = random_split(dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
+
+    # Create data loaders
+    loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
+    train_loader = DataLoader(train_set, shuffle=True, **loader_args)
+    val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
+
+    # (Initialize logging)
+    experiment = wandb.init(project='UNet', resume='allow')
+    experiment.config.update(
+        dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
+             val_percent=val_percent, save_checkpoint=save_checkpoint, amp=amp)
+    )
+    experiment.config.update(extra_config)
+
+    logging.info(f"""Starting training:
+        Epochs:          {epochs}
+        Batch size:      {batch_size}
+        Learning rate:   {learning_rate}
+        Training size:   {n_train}
+        Validation size: {n_val}
+        Checkpoints:     {save_checkpoint}
+        Device:          {device.type}
+        Mixed Precision: {amp}
+    """)
+
+    optimizer = optim.RMSprop(model.parameters(),
+                              lr=learning_rate, weight_decay=weight_decay, momentum=momentum, foreach=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)  # goal: maximize Dice score
+    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    criterion = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
+    global_step = 0
+
+    # Begin training
+    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    global_step = 0
+    for epoch in range(1, epochs + 1):
+        model.train()
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                images, true_masks = batch['image'], batch['mask']
+
+                assert images.shape[1] == model.n_channels, \
+                    f'Network has been defined with {model.n_channels} input channels, ' \
+                    f'but loaded images have {images.shape[1]} channels. Please check that ' \
+                    'the images are loaded correctly.'
+
+                images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+                true_masks = true_masks.to(device=device, dtype=torch.long)
+
+                with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+                    masks_pred = model(images)
+                    if model.n_classes == 1:
+                        loss = criterion(masks_pred.squeeze(1), true_masks.float())
+                        loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), true_masks.float(), multiclass=False)
+                    else:
+                        loss = criterion(masks_pred, true_masks)
+                        loss += dice_loss(
+                            F.softmax(masks_pred, dim=1).float(),
+                            F.one_hot(true_masks, model.n_classes).permute(0, 3, 1, 2).float(),
+                            multiclass=True
+                        )
+
+                optimizer.zero_grad(set_to_none=True)
+                grad_scaler.scale(loss).backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
+
+                pbar.update(images.shape[0])
+                global_step += 1
+                epoch_loss += loss.item()
+                experiment.log({
+                    'train loss': loss.item(),
+                    'step': global_step,
+                    'epoch': epoch
+                })
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
+
+                # Evaluation round
+                division_step = (n_train // (5 * batch_size))
+                if division_step > 0:
+                    if global_step % division_step == 0:
+                        histograms = {}
+                        for tag, value in model.named_parameters():
+                            tag = tag.replace('/', '.')
+                            if not (torch.isinf(value) | torch.isnan(value)).any():
+                                histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
+                            if not (torch.isinf(value.grad) | torch.isnan(value.grad)).any():
+                                histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
+
+                        val_score = evaluate(model, val_loader, device, amp)
+                        scheduler.step(val_score)
+
+                        logging.info('Validation Dice score: {}'.format(val_score))
+                        try:
+                            experiment.log({
+                                'learning rate': optimizer.param_groups[0]['lr'],
+                                'validation Dice': val_score,
+                                'images': wandb.Image(images[0].cpu()),
+                                'masks': {
+                                    'true': wandb.Image(true_masks[0].float().cpu()),
+                                    'pred': wandb.Image(masks_pred.argmax(dim=1)[0].float().cpu()),
+                                },
+                                'step': global_step,
+                                'epoch': epoch,
+                                **histograms
+                            })
+                        except:
+                            pass
+
+        if save_checkpoint:
+            Path(CHECKPOINT_DIR).mkdir(parents=True, exist_ok=True)
+            state_dict = model.state_dict()
+            state_dict['mask_values'] = dataset.mask_values
+            torch.save(state_dict, str(CHECKPOINT_DIR / 'checkpoint_epoch{}.pth'.format(epoch)))
+            logging.info(f'Checkpoint {epoch} saved!')
+
+
+def train_unet(device: torch.device, args):
+    logging.info("Training UNet")
+
+    img_scale = 0.5
+    model = UNet(n_channels=3, n_classes=2, bilinear=False)
+    model = model.to(memory_format=torch.channels_last)
+
+    logging.info(f'Network:\n'
+                f'\t{model.n_channels} input channels\n'
+                f'\t{model.n_classes} output channels (classes)\n'
+                f'\t{"Bilinear" if model.bilinear else "Transposed conv"} upscaling')
+
+    model.to(device=device)
+    extra_config = dict(img_scale=img_scale)
+
+    try:
+        dataset = CarvanaDataset(IMAGE_DIR, MASK_DIR, img_scale)
+    except (AssertionError, RuntimeError, IndexError):
+        dataset = BasicDataset(MASK_DIR, IMAGE_DIR, img_scale)
+
+    try:
+        train_model(
+            model=model,
+            device=device,
+            dataset=dataset,
+            extra_config=extra_config
+        )
+    except torch.cuda.OutOfMemoryError:
+        logging.error("Detected OutOfMemoryError! "
+                      "Enabling checkpointing to reduce memory usage, but this slows down training. "
+                      "Consider enabling AMP (--amp) for fast and memory efficient training")
+        torch.cuda.empty_cache()
+        model.use_checkpointing()
+        train_model(
+            model=model,
+            device=device,
+            dataset=dataset,
+            extra_config=extra_config
+        )
+
+
+def mask_to_image(mask: np.ndarray, mask_values):
+    if isinstance(mask_values[0], list):
+        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8)
+    elif mask_values == [0, 1]:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=bool)
+    else:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=np.uint8)
+
+    if mask.ndim == 3:
+        mask = np.argmax(mask, axis=0)
+
+    for i, v in enumerate(mask_values):
+        out[mask == i] = v
+
+    return Image.fromarray(out)
+
+
+def _generate_name(fn):
+    return f'{os.path.splitext(fn)[0]}_OUT.png'
+
+
+def predict_img(model, device: torch.device, full_img, scale_factor=1, out_threshold=0.5):
+    model.eval()
+    img = torch.from_numpy(BasicDataset.preprocess(None, full_img, scale_factor, is_mask=False))
+    img = img.unsqueeze(0)
+    img = img.to(device=device, dtype=torch.float32)
+
+    with torch.no_grad():
+        output = model(img).cpu()
+        output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode="bilinear")
+        if model.n_classes > 1:
+            mask = output.argmax(dim=1)
+        else:
+            mask = torch.sigmoid(output) > out_threshold
+    
+    return mask[0].long().squeeze().numpy()
+
+
+def unet_predict(device, args):
+
+    input_files = args.input
+    output_files = args.output or list(map(_generate_name, args.input))
+
+    model = UNet(n_channels=3, n_classes=2, bilinear=False)
+    model.to(device=device)
+    
+    state_dict = torch.load(args.model_path, map_location=device)
+    mask_values = state_dict.pop("mask_values", [0, 1])
+    # state_dict  = torch.hub.load_state_dict_from_url("https://github.com/milesial/Pytorch-UNet/releases/download/v3.0/unet_carvana_scale0.5_epoch2.pth", map_location="cpu", progress=True)
+    # if 'mask_values' in state_dict:
+    #     state_dict.pop('mask_values')
+
+    mask_values = [0, 1]
+    model.load_state_dict(state_dict)
+    logging.info(f"Model loaded from {args.model_path}")
+
+    for i, filename in enumerate(input_files):
+        logging.info(f"Predicting {filename}")
+        img = Image.open(filename)
+        mask = predict_img(model, device, img)
+
+        if not args.no_save:
+            output_filename = output_files[i]
+            result = mask_to_image(mask, mask_values)
+            result.save(output_filename)
+            logging.info(f"Mask saved to {output_filename}")
+        
+        if args.display:
+            logging.info(f"Displaying results for {filename}, close to continue...")
+            plot_img_and_mask(img, mask)
--- train.py
+++ train.py
@@ -1,39 +1,32 @@
+import argparse
+import logging
+import sys
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from model import TestNet
-from models import TestNet
 
-def train_model(model: nn.Module, model_name: str, train_loader, optimizer, criterion, epochs: int):
-    for epoch in range(epochs):
-        for i, (data, labels) in enumerate(train_loader):
-            outputs = model(data)
-            loss = criterion(outputs, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            if (i+1) % 10 == 0:
-                print(f'{model_name} Epoch [{epoch+1}/{epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+from GPT.char_gpt import train_char_gpt
+from UNet.unet import train_unet
 
-    # Save the model
-    torch.save(model.state_dict(), f"{model_name}.pth")
+def main(args):
+    parser = argparse.ArgumentParser(description="Train a model")
+    models = parser.add_subparsers(dest="model")
+    
+    unet_parser = models.add_parser("unet", help="Train a UNet")
+    unet_parser.set_defaults(train=train_unet)
+
+    
+    args = parser.parse_args()
+    if not hasattr(args, "train"):
+        parser.print_help()
+        return 1
+
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f'Using device {device}')
+
+    args.train(device, args)
+    return 0
+
 
 if __name__ == "__main__":
-    input_dim = 2
-    output_dim = 2
-    batch_size = 32
-    learning_rate = 0.001
-    epochs = 10
-
-    train_data = torch.randn(1000, input_dim)
-    train_labels = (train_data.sum(dim=1) > 0).long()
-
-    train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-
-    model = TestNet(input_dim, output_dim)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    train_model(model, "TestNet", train_loader, optimizer, criterion, epochs)
+    sys.exit(main(sys.argv[1:]))
