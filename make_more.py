5f50096b035deba37ae0e60bc532d3a47b98a448
R.Keelan <R.Keelan@gmail.com>
2024-07-09 20:58:09 -0400

    Add evaluation to make_more

--- make_more.py
+++ make_more.py
@@ -16,8 +16,7 @@ def show_plot(N, itos):
     plt.axis('off')
     plt.show()
 
-
-def explore_make_more(args):
+def make_model():
     words = open('data/names.txt', 'r').read().splitlines()
     chars = sorted(list(set(''.join(words))))
     stoi = {s:i+1 for i,s in enumerate(chars)}
@@ -31,22 +30,14 @@ def explore_make_more(args):
             ix1 = stoi[ch1]
             ix2 = stoi[ch2]
             N[ix1, ix2] += 1
-
-    # show_plot(N, itos)
-    
-    g = torch.Generator().manual_seed(2147483647)
-    p = torch.rand(3, generator=g)
-    p = p / p.sum()
-    # print(p)
-    # print(torch.multinomial(p, num_samples=100, replacement=True, generator=g))
-
-    p = N[0].float()
-    p = p / p.sum()
-    # print(p)
-
-    g = torch.Generator().manual_seed(2147483647)
-    P = N.float()
+    P = (N+1).float() # Add 1 to avoid dividing by zero (model smoothing)
     P /= P.sum(1, keepdim=True) # Sum along the rows
+    return P, N, stoi, itos, words
+
+
+def explore_make_more(args):
+    P, N, stoi, itos, words = make_model()
+    g = torch.Generator().manual_seed(2147483647)
     for _ in range(10):
         out = []
         ix = 0
@@ -58,12 +49,39 @@ def explore_make_more(args):
                 break
         print(''.join(out))
 
+def evaluate_make_more(args):
+    P, N, stoi, itos, words = make_model()
+    g = torch.Generator().manual_seed(2147483647)
+    log_likelihood = 0.0
+    n = 0
+    # for w in words:
+    for w in ["andrejq"]:
+        chs = ['.'] + list(w) + ['.']
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            prob = P[ix1, ix2]
+            logprob = torch.log(prob)
+            log_likelihood += logprob
+            n += 1
+            # print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+    print(f'{log_likelihood=}')
+    negative_log_likelihood = -log_likelihood
+    normalized_log_likelihood = negative_log_likelihood / n
+    print(f'{negative_log_likelihood=}')
+    print(f'{normalized_log_likelihood=}')
+
+
+
 def main(args):
     parser = argparse.ArgumentParser(description="Explore data")
     commands = parser.add_subparsers(dest="cmd")
 
     explore_cmd = commands.add_parser("explore", help="Explore MakeMore data")
     explore_cmd.set_defaults(action=explore_make_more)
+
+    evaluate_cmd = commands.add_parser("eval", help="Evaluate MakeMore data")
+    evaluate_cmd.set_defaults(action=evaluate_make_more)
     
     args = parser.parse_args()
     if not hasattr(args, "action"):
